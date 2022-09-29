36e0999d5cb572bde069eb006675ff693f96e94e
Hieu Ngo <hieu.ngo12989@gmail.com>
2022-09-28 17:16:14 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -3,15 +3,21 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int auto_increment,
+  username varchar(255),
+  text varchar(255),
+  date date,
+  roomname varchar(20),
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
-
+CREATE TABLE rooms (
+  id int auto_increment,
+  roomname varchar(20),
+  PRIMARY KEY (id)
+)
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
-
